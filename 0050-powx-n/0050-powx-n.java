f5564466a15637c188f18b7f07443f5e8b41cb32
Soumya Babasaheb Kudache <soumyakudache7@gmail.com>
2026-08-13 15:48:27 +0530

    Time: 0 ms (100%), Space: 48.3 MB (22.83%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,32 +1,23 @@
 class Solution {
     public double myPow(double x, int n) {
 
-        long power = n;  // long handles Integer.MIN_VALUE
+        if (n == 0) {
+            return 1;
+        }
+
+        long power = n;
 
         if (power < 0) {
             x = 1 / x;
             power = -power;
         }
 
-        return power(x, power);
-    }
+        double half = myPow(x, (int)(power / 2));
 
-    public double power(double x, long n) {
-
-        // Base case
-        if (n == 0) {
-            return 1;
-        }
-
-        // Recursive call
-        double half = power(x, n / 2);
-
-        // Even power
-        if (n % 2 == 0) {
+        if (power % 2 == 0) {
             return half * half;
         }
 
-        // Odd power
         return x * half * half;
     }
 }
